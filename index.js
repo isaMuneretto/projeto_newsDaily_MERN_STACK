2aca31b66883bda8cba5f9d54d651b1d4f739777
isaMuneretto <isamuneretto@gmail.com>
2023-09-05 14:32:56 -0300

    add route soma

--- index.js
+++ index.js
@@ -2,7 +2,23 @@ const express = require('express');
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello World")
+  const soma = 100 + 1;
+
+  res.send({soma: soma})
 });
+
+//ROTA é a porta de entrada da nossa API, do nosso backend
+//Method HTTP - É a forma como a internet se comunica - CRUD (CREATE, READ, UPDATE, DELETE)
+    // GET - Pega uma info
+    // POST - Cria uma info
+    // PUT - Altera toda a info. Ex: Atualiza todo o obj criado.
+    // PATCH - Altera parte da info. Ex: Altera só o nome do obj.
+    // DELETE - Apaga uma info
+
+//Name('/') - É um identificador da rota
+
+//Function (Callback "função executado por trás de outra função") "(req, res)" - Responsavel por executar algum comando
+
+//header é um metadado que é o dado dos dados. É onde configura os dados
 
 app.listen(3000);
