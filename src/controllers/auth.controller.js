2768e6b2ca6766d11a016756833f52b76a4d944b
isaMuneretto <isamuneretto@gmail.com>
2023-10-08 14:26:06 -0300

    aplicando JWT

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,5 +1,5 @@
 import bcrypt from "bcrypt";
-import { loginService } from "../services/auth.service.js";
+import { loginService, generateToken } from "../services/auth.service.js";
 
 const login = async (req, res) => {
     const { email, password } = req.body; //formulario a ser preenchido é o de login que recebe pelo body
@@ -17,7 +17,9 @@ const login = async (req, res) => {
             res.status(400).send({ message: "User or password not found!"})
         }
 
-        res.send("Login ok")
+        const token = generateToken(user.id); //não precisa ser async 
+
+        res.send({token}); //envia para o usuario o token do login
     } catch (err) {
         res.status(500).send(err.message);
     }
--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,7 +1,11 @@
 //comunica com o banco de dados atraves da model
 
 import User from "../models/User.js";
+import jwt from "jsonwebtoken";
 
-const loginService = (email) => User.findOne({email: email}).select("+password"); //chaves é o filtro para buscar na model User
+const loginService = (email) => User.findOne({ email: email }).select("+password"); //chaves é o filtro para buscar na model User
 
-export {loginService};
+const generateToken = (id) => jwt.sign({ id: id }, process.env.SECRET_JWT, { expiresIn: 86400 });
+/* guarda a sessão do usuario e o front-end saber qual é o usuario sem expor os dados do usuario (JWT)
+os três parametros são: o dado que é recebido, a chave secreta fica no .env (criptografia MD5 "Hash") e terceiro são as options(tempo expiração do token) ) */
+export { loginService, generateToken };
