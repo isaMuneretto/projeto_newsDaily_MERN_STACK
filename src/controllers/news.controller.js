170352f5fa33a38b5585289cf474bf6fc02e4fbf
isaMuneretto <isamuneretto@gmail.com>
2023-10-12 15:46:39 -0300

    configurando header

--- src/controllers/news.controller.js
+++ src/controllers/news.controller.js
@@ -2,6 +2,24 @@ import { createService, findAllService } from "../services/news.service.js";
 
 const create = async (req, res) => {
     try {
+        const { authorization } = req.headers;
+        
+        if (!authorization) {
+            return res.send(401);
+        }
+
+        const parts = authorization.split(" ");
+
+        if (parts.length !== 2) {
+            return res.send(401);
+        }
+
+        const [schema, token] = parts;
+        
+        if (schema !== "Bearer") {
+            return res.send(401);
+        }
+
         const { title, text, banner } = req.body;
 
         if (!title || !text || !banner) {
@@ -12,7 +30,7 @@ const create = async (req, res) => {
             title,
             text,
             banner,
-            id: "objectidfake",
+            user: { _id: "6521986a636e97d975164f3e" },
         });
 
         res.send(201);
@@ -21,9 +39,14 @@ const create = async (req, res) => {
     }
 };
 
-const findAll = (req, res) => {
-    const news = [];
+const findAll = async (req, res) => {
+    const news = await findAllService();
+    if (news.length === 0) {
+        return res.status(400).send({
+            message: "There are no registered news",
+        });
+    }
     res.send(news);
 };
 
-export default { create, findAll };
+export { create, findAll };
